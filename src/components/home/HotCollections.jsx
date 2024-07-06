bf09d41be3daa5f8fa82cc46e1b3e8c0fc3bc9e3
adam-gill <adam.douglas.gill@gmail.com>
2024-07-06 12:01:48 -0400

    added various animations to home page

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -6,9 +6,11 @@ import Arrow from "../UI/Arrow.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const HotCollections = () => {
+  AOS.init();
   const [collections, setCollections] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -20,11 +22,9 @@ const HotCollections = () => {
       );
       setCollections(data);
       setLoading(false);
-
     } catch (error) {
       console.log(error);
       setLoading(true);
-
     }
   }
 
@@ -47,24 +47,24 @@ const HotCollections = () => {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: false
-        }
+          dots: false,
+        },
       },
       {
         breakpoint: 800,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   function NextArrow(props) {
@@ -72,7 +72,12 @@ const HotCollections = () => {
     return (
       <div
         className={className}
-        style={{ ...style, filter: 'invert()', transform: "scale(1.4)", right: "-5px" }}
+        style={{
+          ...style,
+          filter: "invert()",
+          transform: "scale(1.4)",
+          right: "-5px",
+        }}
         onClick={onClick}
       />
     );
@@ -84,103 +89,125 @@ const HotCollections = () => {
       <div
         className={className}
         style={{
-          ...style, filter: 'invert()', transform: "scale(1.4)", left: "-5px"
-        }
-        }
+          ...style,
+          filter: "invert()",
+          transform: "scale(1.4)",
+          left: "-5px",
+        }}
         onClick={onClick}
       />
     );
   }
-
-
 
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <div className="text-center">
+            <div
+              className="text-center"
+              data-aos="fade-in"
+              data-aos-duration="1500"
+            >
               <h2>Hot Collections</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-
-          {loading ? (<Slider {...settings}>
-            {new Array(4).fill(0).map((_, index) => (
-              <div
-                className="col-lg-12 col-md-12 col-sm-10 col-xs-12"
-                key={index}
-              >
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to="/item-details">
+          <div data-aos="fade-up" data-aos-duration="1000">
+            {loading ? (
+              <Slider {...settings}>
+                {new Array(4).fill(0).map((_, index) => (
+                  <div
+                    className="col-lg-12 col-md-12 col-sm-10 col-xs-12"
+                    key={index}
+                  >
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to="/item-details">
+                          <div
+                            className="skeleton-box"
+                            style={{ width: "100%", height: "200px" }}
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to={"/author"}>
+                          <div
+                            className="skeleton-box"
+                            style={{
+                              width: "50px",
+                              height: "50px",
+                              borderRadius: "50%",
+                            }}
+                          ></div>
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
                       <div
-                        className="skeleton-box"
-                        style={{ width: '100%', height: '200px' }}
-                      />
-                    </Link>
+                        className="nft_coll_info"
+                        style={{ display: "flex", flexDirection: "column" }}
+                      >
+                        <Link to="/explore">
+                          <div
+                            className="skeleton-box"
+                            style={{ width: "100px", height: "20px" }}
+                          ></div>
+                        </Link>
+                        <span>
+                          {" "}
+                          <div
+                            className="skeleton-box"
+                            style={{ width: "60px", height: "20px" }}
+                          ></div>
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                  <div className="nft_coll_pp">
-                    <Link to={"/author"}>
-                      <div className="skeleton-box" style={{ width: '50px', height: '50px', borderRadius: '50%' }}></div>
-                    </Link>
-                    <i className="fa fa-check"></i>
+                ))}
+              </Slider>
+            ) : (
+              <Slider {...settings}>
+                {Object.values(collections).map((item) => (
+                  <div
+                    className="col-lg-12 col-md-12 col-sm-10 col-xs-12"
+                    key={item.id}
+                  >
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to={"/item-details/" + item.nftId}>
+                          <img
+                            src={item.nftImage}
+                            className="lazy img-fluid h-full"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to={"/author/" + item.authorId}>
+                          <img
+                            className="lazy pp-coll"
+                            src={item.authorImage}
+                            alt=""
+                          />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{item.title}</h4>
+                        </Link>
+                        <span>ERC-{item.code}</span>
+                      </div>
+                    </div>
                   </div>
-                  <div className="nft_coll_info" style={{ display: 'flex', flexDirection: 'column' }}>
-                    <Link to="/explore">
-                      <div className="skeleton-box" style={{ width: '100px', height: '20px' }}></div>
-
-                    </Link>
-                    <span>                      <div className="skeleton-box" style={{ width: '60px', height: '20px' }}></div>
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-            ))}
-          </Slider>) : (<Slider {...settings}>
-            {Object.values(collections).map((item) => (
-              <div
-                className="col-lg-12 col-md-12 col-sm-10 col-xs-12"
-                key={item.id}
-              >
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to={"/item-details/" + item.nftId}>
-                      <img
-                        src={item.nftImage}
-                        className="lazy img-fluid h-full"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft_coll_pp">
-                    <Link to={"/author/" + item.authorId}>
-                      <img
-                        className="lazy pp-coll"
-                        src={item.authorImage}
-                        alt=""
-                      />
-                    </Link>
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="nft_coll_info">
-                    <Link to="/explore">
-                      <h4>{item.title}</h4>
-                    </Link>
-                    <span>ERC-{item.code}</span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </Slider>)}
-
-
+                ))}
+              </Slider>
+            )}
+          </div>
         </div>
-      </div >
-    </section >
+      </div>
+    </section>
   );
 };
-
 
 export default HotCollections;
--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 
 const Landing = () => {
+
+  AOS.init();
+
   return (
     <section
       id="section-hero"
@@ -18,25 +24,25 @@ const Landing = () => {
             <div className="col-md-6">
               <div className="spacer-single"></div>
               <h6>
-                <span className="text-uppercase id-color-2">
+                <span className="text-uppercase id-color-2" data-aos="fade-in" data-aos-duration="1500">
                   Ultraverse Market
                 </span>
               </h6>
               <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
+              <h1 data-aos="fade-up" data-aos-duration="1500">Create, sell or collect digital items.</h1>
+              <p className="lead" data-aos="fade-up" data-aos-duration="1500">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
                 interchangeable
               </p>
               <div className="spacer-10"></div>
-              <Link className="btn-main lead" to="/explore">
+              <Link className="btn-main lead" to="/explore" data-aos="fade-in" data-aos-duration="1500" data-aos-delay="1000">
                 Explore
               </Link>
               <div className="mb-sm-30"></div>
             </div>
             <div className="col-md-6 xs-hide">
-              <img src={NFT} className="lazy img-fluid" alt="" />
+              <img src={NFT} className="lazy img-fluid" alt="" data-aos="fade-in" data-aos-duration="2000" data-aos-delay="1000" />
             </div>
           </div>
         </div>
