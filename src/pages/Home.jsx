f09375a9dceccda458597c845ee62339cb08d0b3
adam-gill <adam.douglas.gill@gmail.com>
2024-07-06 12:08:21 -0400

    merge conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,8 @@ const Home = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  // From main, don't remove
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
